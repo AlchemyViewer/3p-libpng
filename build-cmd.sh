d759dc607a9564e837b910785ba75b1be018cb04
Rye Mutt <rye@alchemyviewer.org>
2022-01-26 11:12:25 -0500

    Fix

--- build-cmd.sh
+++ build-cmd.sh
@@ -252,7 +252,7 @@ pushd "$PNG_SOURCE_DIR"
                     -DPNG_SHARED=OFF \
                     -DPNG_HARDWARE_OPTIMIZATIONS=ON \
                     -DPNG_HARDWARE_ARM=ON \
-                    -DPNG_ARM_NEON=ON \
+                    -DPNG_ARM_NEON=on \
                     -DPNG_BUILD_ZLIB=ON \
                     -DZLIB_INCLUDE_DIR="${stage}/packages/include/zlib" \
                     -DZLIB_LIBRARY="${stage}/packages/lib/debug/libz.a"
